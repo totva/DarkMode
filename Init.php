416758abbbab4ddaaf7471f68022d69e2b2c9d6d
totva <16940095+totva@users.noreply.github.com>
2020-10-15 01:49:59 +0200

    fix Init.php

--- Init.php
+++ Init.php
@@ -5,6 +5,10 @@ use FacturaScripts\Core\Base\InitClass;
 
 class Init extends InitClass
 {
+	public function init()
+    {
+        /// código a ejecutar cada vez que carga FacturaScripts (si este plugin está activado).
+    }
 
     public function update()
     {
